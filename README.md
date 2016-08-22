642dc1037d94494161d784fb1d1077985ae34f06
Rommel Rico <rommeltj@gmail.com>
2016-08-22 10:17:50 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,19 @@
+StarCitizenFacts
+=======================
+
+Alexa Skills Kit app that returns a random fact from the Star Citizen Universe.
+
+Documentation  
+-----------------------
+
+Soon.  
+
+Terms of Use and Contributions
+-----------------------
+
+Star Citizen is intellectual property Cloud Imperium Games Corporation, Roberts Space Industries Corp., Cloud Imperium Games UK Ltd., and Roberts Space Industries International Ltd. Use of this intellectual property is allowed by their Terms of Service. For details, please refer to: https://robertsspaceindustries.com/tos
+
+You can do what you want with my code.
+
+Contributions are welcome. Please submit a Pull request and I'll review as soon as possible.
+
---
+++ src/main/java/com/rommelrico/alexa/starcitizenfacts/StarCitizenFactsSpeechlet.java
@@ -0,0 +1,138 @@
+/**
+    Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+
+        http://aws.amazon.com/apache2.0/
+
+    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+package com.rommelrico.alexa.starcitizenfacts;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.amazon.speech.slu.Intent;
+import com.amazon.speech.speechlet.IntentRequest;
+import com.amazon.speech.speechlet.LaunchRequest;
+import com.amazon.speech.speechlet.Session;
+import com.amazon.speech.speechlet.SessionEndedRequest;
+import com.amazon.speech.speechlet.SessionStartedRequest;
+import com.amazon.speech.speechlet.Speechlet;
+import com.amazon.speech.speechlet.SpeechletException;
+import com.amazon.speech.speechlet.SpeechletResponse;
+import com.amazon.speech.ui.PlainTextOutputSpeech;
+import com.amazon.speech.ui.Reprompt;
+import com.amazon.speech.ui.SimpleCard;
+
+/**
+ * This simple sample has no external dependencies or session management, and shows the most basic
+ * example of how to handle Alexa Skill requests.
+ */
+public class StarCitizenFactsSpeechlet implements Speechlet {
+    private static final Logger log = LoggerFactory.getLogger(StarCitizenFactsSpeechlet.class);
+
+    /**
+     * Array containing Star Citizen facts.
+     */
+    private static final String[] STAR_CITIZEN_FACTS = new String[] {
+        "TODO",
+        "TODO"
+    };
+
+    @Override
+    public void onSessionStarted(final SessionStartedRequest request, final Session session)
+            throws SpeechletException {
+        log.info("onSessionStarted requestId={}, sessionId={}", request.getRequestId(),
+                session.getSessionId());
+        // any initialization logic goes here
+    }
+
+    @Override
+    public SpeechletResponse onLaunch(final LaunchRequest request, final Session session)
+            throws SpeechletException {
+        log.info("onLaunch requestId={}, sessionId={}", request.getRequestId(),
+                session.getSessionId());
+        return getNewFactResponse();
+    }
+
+    @Override
+    public SpeechletResponse onIntent(final IntentRequest request, final Session session)
+            throws SpeechletException {
+        log.info("onIntent requestId={}, sessionId={}", request.getRequestId(),
+                session.getSessionId());
+
+        Intent intent = request.getIntent();
+        String intentName = (intent != null) ? intent.getName() : null;
+
+        if ("GetStarCitizenFactIntent".equals(intentName)) {
+            return getNewFactResponse();
+
+        } else if ("AMAZON.HelpIntent".equals(intentName)) {
+            return getHelpResponse();
+
+        } else if ("AMAZON.StopIntent".equals(intentName)) {
+            PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
+            outputSpeech.setText("Goodbye");
+
+            return SpeechletResponse.newTellResponse(outputSpeech);
+        } else if ("AMAZON.CancelIntent".equals(intentName)) {
+            PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
+            outputSpeech.setText("Goodbye");
+
+            return SpeechletResponse.newTellResponse(outputSpeech);
+        } else {
+            throw new SpeechletException("Invalid Intent");
+        }
+    }
+
+    @Override
+    public void onSessionEnded(final SessionEndedRequest request, final Session session)
+            throws SpeechletException {
+        log.info("onSessionEnded requestId={}, sessionId={}", request.getRequestId(),
+                session.getSessionId());
+        // any cleanup logic goes here
+    }
+
+    /**
+     * Gets a random new fact from the list and returns to the user.
+     */
+    private SpeechletResponse getNewFactResponse() {
+        // Get a random Star Citizen fact from the Star Citizen facts list
+        int factIndex = (int) Math.floor(Math.random() * STAR_CITIZEN_FACTS.length);
+        String fact = STAR_CITIZEN_FACTS[factIndex];
+
+        // Create speech output
+        String speechText = "Here's your Star Citizen fact: " + fact;
+
+        // Create the Simple card content.
+        SimpleCard card = new SimpleCard();
+        card.setTitle("Star Citizen Facts");
+        card.setContent(speechText);
+
+        // Create the plain text output.
+        PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+        speech.setText(speechText);
+
+        return SpeechletResponse.newTellResponse(speech, card);
+    }
+
+    /**
+     * Returns a response for the help intent.
+     */
+    private SpeechletResponse getHelpResponse() {
+        String speechText =
+                "You can ask Star Citizen Facts tell me a Star Citizen fact, or, you can say exit. What can I "
+                        + "help you with?";
+
+        // Create the plain text output.
+        PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+        speech.setText(speechText);
+
+        // Create reprompt
+        Reprompt reprompt = new Reprompt();
+        reprompt.setOutputSpeech(speech);
+
+        return SpeechletResponse.newAskResponse(speech, reprompt);
+    }
+}
